d6a65bf4bc5b3e6746f01ce233857d973a7cc738
Alexandre R. <alexandreregalado@hotmail.com>
2026-03-25 12:51:50 +0000

    feat: add course management module with serializers, views, and service

---
+++ src/apis/competition-api-v2/admin_api/modules/courses/serializers.py
@@ -0,0 +1,44 @@
+"""
+Course management serializers
+"""
+
+from rest_framework import serializers
+
+
+# Helpers
+class _NucleoSummarySerializer(serializers.Serializer):
+    name = serializers.CharField()
+    abbreviation = serializers.CharField()
+
+
+# Response serializers
+class CourseListSerializer(serializers.Serializer):
+    """Serializer for listing courses"""
+
+    id = serializers.UUIDField()
+    name = serializers.CharField()
+    abbreviation = serializers.CharField()
+    nucleo = _NucleoSummarySerializer()
+
+
+class CourseDetailSerializer(CourseListSerializer):
+    """Serializer for course details"""
+
+    ...
+
+
+# Request serializers
+class CourseCreateSerializer(serializers.Serializer):
+    """Serializer for creating a course"""
+
+    name = serializers.CharField(required=True)
+    abbreviation = serializers.CharField(required=True)
+    nucleo_id = serializers.UUIDField(required=True)
+
+
+class CourseUpdateSerializer(serializers.Serializer):
+    """Serializer for updating a course"""
+
+    name = serializers.CharField(required=False)
+    abbreviation = serializers.CharField(required=False)
+    nucleo_id = serializers.UUIDField(required=False)
--- src/apis/competition-api-v2/admin_api/urls.py
+++ src/apis/competition-api-v2/admin_api/urls.py
@@ -1,5 +1,6 @@
 from admin_api.modules.admins import urlpatterns as admin_urls
 from admin_api.modules.athletes import urlpatterns as athletes_urls
+from admin_api.modules.courses import urlpatterns as courses_urls
 from admin_api.modules.modality_types import urlpatterns as modality_type_urls
 from admin_api.modules.nucleos import urlpatterns as nucleos_urls
 from django.urls import include, path
@@ -13,6 +14,7 @@ urlpatterns = [
     path("modality-types/", include(modality_type_urls)),
     path("athletes/", include(athletes_urls)),
     path("nucleos/", include(nucleos_urls)),
+    path("courses/", include(courses_urls)),
     # path("modalities/", include(modality_urls)),
     # path("tournaments/", include(tournament_urls)),
 ]
