efe20cb6933b6328477d7c7cf06948dea5be8c12
Alexandre R. <alexandreregalado@hotmail.com>
2026-04-28 01:55:37 +0100

    fix: update navigateBack function to check history state correctly

--- src/frontend/admin-panel/src/utils.tsx
+++ src/frontend/admin-panel/src/utils.tsx
@@ -1,6 +1,8 @@
 export const navigateBack = ( navigate: any, defaultPath: string) => {
 
-  if (window.history.length > 1 && document.referrer !== window.location.href) {
+
+  // Verifica se há histórico para voltar e se o referenciador é diferente da URL atual
+  if (window.history.length > 1 && window.history.state.idx > 0) {
     window.history.back();
   } else {
     navigate(defaultPath);
