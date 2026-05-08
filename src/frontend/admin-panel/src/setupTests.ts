07086a7501b3a9b1f97410a3fa2e654284dc8a0e
DiegoA4476 <diegoaguilar@ua.pt>
2026-05-08 16:17:10 +0100

    initial set up for integration tests

---
+++ src/frontend/admin-panel/src/setupTests.ts
@@ -0,0 +1,26 @@
+import '@testing-library/jest-dom'
+import 'whatwg-fetch'
+import { server } from './mocks/server'
+
+
+// Create a simple mock for localStorage
+const localStorageMock = (function () {
+  let store: Record<string, string> = {}
+  return {
+    getItem: (key: string) => store[key] || null,
+    setItem: (key: string, value: string) => { store[key] = value.toString() },
+    removeItem: (key: string) => { delete store[key] },
+    clear: () => { store = {} },
+  }
+})()
+
+// Assign it to the global window object
+Object.defineProperty(window, 'localStorage', {
+  value: localStorageMock,
+})
+
+
+// Start MSW before all tests and close after
+beforeAll(() => server.listen({ onUnhandledRequest: 'warn' }))
+afterEach(() => server.resetHandlers())
+afterAll(() => server.close())
