19cac2485e2a1fb1f3b2b602d3c469d94572a63f
DiegoA4476 <diegoaguilar@ua.pt>
2026-05-12 19:02:12 +0100

    small modifications in test_utils (public-website)

--- src/frontend/public-website/src/test-utils.tsx
+++ src/frontend/public-website/src/test-utils.tsx
@@ -2,13 +2,9 @@ import type { ReactElement } from 'react'
 import { render } from '@testing-library/react'
 import { BrowserRouter } from 'react-router-dom'
 
-type RenderOptions = {
-  initialEntries?: string[]
-}
 
 export function renderWithRouter(
   ui: ReactElement,
-  { initialEntries = ['/'] }: RenderOptions = {},
 ) {
   return render(
     <BrowserRouter>
