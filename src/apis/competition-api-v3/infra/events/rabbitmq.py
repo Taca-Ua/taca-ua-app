2b4b2882e2539fec995a87a19f27cbfe774100ab
Alexandre R. <alexandreregalado@hotmail.com>
2026-06-15 01:06:21 +0100

    refactor: enhance RabbitMQClient connection handling and error recovery

--- src/apis/competition-api-v3/infra/events/rabbitmq.py
+++ src/apis/competition-api-v3/infra/events/rabbitmq.py
@@ -2,10 +2,16 @@ import json
 
 import pika
 from django.conf import settings
+from pika.exceptions import AMQPError
 
 
 class RabbitMQClient:
     def __init__(self):
+        self.connection = None
+        self.channel = None
+        self.connect()
+
+    def connect(self):
         credentials = pika.PlainCredentials(
             settings.RABBITMQ_USER,
             settings.RABBITMQ_PASSWORD,
@@ -16,16 +22,30 @@ class RabbitMQClient:
                 host=settings.RABBITMQ_HOST,
                 port=settings.RABBITMQ_PORT,
                 credentials=credentials,
+                heartbeat=60,
+                blocked_connection_timeout=300,
             )
-        )
-        print(
-            f"Connected to RabbitMQ at {settings.RABBITMQ_HOST}:{settings.RABBITMQ_PORT} as user '{settings.RABBITMQ_USER}'"
         )
         self.channel = self.connection.channel()
 
     def publish(self, routing_key, body):
-        self.channel.basic_publish(
-            exchange=settings.RABBITMQ_EXCHANGE,
-            routing_key=routing_key,
-            body=json.dumps(body),
-        )
+        try:
+            if (
+                self.connection is None
+                or self.connection.is_closed
+                or self.channel.is_closed
+            ):
+                self.connect()
+
+            self.channel.basic_publish(
+                exchange=settings.RABBITMQ_EXCHANGE,
+                routing_key=routing_key,
+                body=json.dumps(body),
+            )
+        except AMQPError:
+            self.connect()
+            self.channel.basic_publish(
+                exchange=settings.RABBITMQ_EXCHANGE,
+                routing_key=routing_key,
+                body=json.dumps(body),
+            )
