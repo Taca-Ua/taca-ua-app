09eed88880fb7a1a3cbec95a0745b19135ac649a
Alexandre R. <alexandreregalado@hotmail.com>
2026-07-11 02:11:08 +0100

    feat: update Regulation model to allow nullable and blank description field

--- src/apis/competition-api-v3/apps/regulations/models.py
+++ src/apis/competition-api-v3/apps/regulations/models.py
@@ -1,18 +1,26 @@
-from django.db import models
 import uuid
 
-from apps.seasons.models import Season
 from apps.modalities.models import Modality
+from apps.seasons.models import Season
+from django.db import models
 
 
 class Regulation(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     file_url = models.CharField(max_length=255)
     title = models.CharField(max_length=255)
-    description = models.TextField()
+    description = models.TextField(null=True, blank=True)
 
-    season = models.ForeignKey(Season, on_delete=models.CASCADE, related_name="regulations")
-    modality = models.OneToOneField(Modality, on_delete=models.CASCADE, related_name="regulation", null=True, blank=True)
+    season = models.ForeignKey(
+        Season, on_delete=models.CASCADE, related_name="regulations"
+    )
+    modality = models.OneToOneField(
+        Modality,
+        on_delete=models.CASCADE,
+        related_name="regulation",
+        null=True,
+        blank=True,
+    )
 
     def __str__(self):
         return f"<Regulation: {self.title}>"
